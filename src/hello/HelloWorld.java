8bf00475c2910cc442b01eb1545fc979af7cefac
Archer Tan <Archer Tan@GOS-01711060843>
2018-05-10 09:06:55 +0800

    新增功能 1.println("hello world");

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -3,6 +3,7 @@ package hello;
 public class HelloWorld {
 	public static void main(String[] args) {
 		println("hello git");
+		println("hello world");
 	}
 	
 	public static void println(String str){
